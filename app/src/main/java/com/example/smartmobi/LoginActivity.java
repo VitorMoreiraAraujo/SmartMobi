c34384104f54112822f168b896bdfee294a0aad0
Vitor Moreira Araujo <vitormoreira.araujo@gmail.com>
2023-04-15 18:51:15 -0300

    'Arrumando' e layout
    'Arrumando' os arquivos para funcionar na minha máquina e arrumando os layout

--- app/src/main/java/com/example/smartmobi/LoginActivity.java
+++ app/src/main/java/com/example/smartmobi/LoginActivity.java
@@ -2,13 +2,52 @@ package com.example.smartmobi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText edtUsuario, edtSenha;
+    Button btnEntrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+
+        edtUsuario = findViewById(R.id.idUsuario);
+        edtSenha = findViewById(R.id.idSenha);
+        btnEntrar = findViewById(R.id.idEntrar);
+
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String usuario, senha;
+
+                usuario = edtUsuario.getText().toString();
+                senha = edtSenha.getText().toString();
+
+                if (usuario.equals("vitor") && senha.equals("22038")) {
+
+                    startActivity(new Intent(getApplicationContext(), MenuPrincipalActivity.class));
+                    finish();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Senha e/ou Usuario errados!!", Toast.LENGTH_LONG).show();
+                    limparJanela();
+                }
+            }
+
+        });
+
+    }
+
+    public void limparJanela() {
+        edtUsuario.setText("");
+        edtSenha.setText("");
+        edtUsuario.requestFocus();
     }
 }
--- app/src/main/java/com/example/smartmobi/MenuPrincipalActivity.java
+++ app/src/main/java/com/example/smartmobi/MenuPrincipalActivity.java
@@ -1,14 +1,29 @@
 package com.example.smartmobi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuPrincipalActivity extends AppCompatActivity {
 
+    CardView cardView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_principal_layout);
+
+        cardView = findViewById(R.id.idCardView);
+
+
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), SobreActivity.class));
+                finish();
+            }
+        });
     }
 }
