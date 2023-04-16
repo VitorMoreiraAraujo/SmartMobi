7f0e92ea43e1f3bfb564ed1eaff9bf29ead4d630
Vitor Moreira Araujo <vitormoreira.araujo@gmail.com>
2023-04-16 16:47:20 -0300

    Tela de transporte com bottom navigation

---
+++ app/src/main/java/com/example/smartmobi/OnibusFragment.java
@@ -0,0 +1,21 @@
+package com.example.smartmobi;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class OnibusFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_onibus_layout, container, false);
+        return view;
+    }
+}
