467e37943ace4838d362568c85ffc44490b585b9
Vitor Moreira Araujo <vitormoreira.araujo@gmail.com>
2023-04-16 18:04:47 -0300

    Terminando a tela Transito com navigation view e seus fragment

---
+++ app/src/main/java/com/example/smartmobi/AlagamentoFragment.java
@@ -0,0 +1,22 @@
+package com.example.smartmobi;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class AlagamentoFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_alagamento_layout, container, false);
+        return view;
+    }
+}
--- app/src/main/java/com/example/smartmobi/TransitoActivity.java
+++ app/src/main/java/com/example/smartmobi/TransitoActivity.java
@@ -1,14 +1,69 @@
 package com.example.smartmobi;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class TransitoActivity extends AppCompatActivity {
+
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.transito_layout);
+
+        drawerLayout = findViewById(R.id.idDrawerLayout);
+        navigationView = findViewById(R.id.idNavigationView);
+
+        /*
+        *ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.opennav, R.string.closenav);
+
+        drawerLayout.addDrawerListener(toggle);
+
+        toggle.syncState();
+        *
+        *
+        *
+        * */
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.mMaisTransito:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.idConteiner, new MaisTransitoFragment()).commit();
+                        break;
+                    case R.id.mMenosTransito:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.idConteiner, new MenosTransitoFragment()).commit();
+                        break;
+                    case R.id.mAlagamento:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.idConteiner, new AlagamentoFragment()).commit();
+                        break;
+
+                }
+                drawerLayout.closeDrawer(GravityCompat.START);
+
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
