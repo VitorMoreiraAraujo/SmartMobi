0c758fe94faf4b6cafb9163b622e22440c43f196
Vitor Moreira Araujo <vitormoreira.araujo@gmail.com>
2023-04-16 17:23:32 -0300

    Criando a tela Bicicletas e suas fragments

---
+++ app/src/main/java/com/example/smartmobi/BicicletasActivity.java
@@ -0,0 +1,16 @@
+package com.example.smartmobi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BicicletasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bicicletas_layout);
+
+        
+    }
+}
---
+++ app/src/main/java/com/example/smartmobi/DicasFragment.java
@@ -0,0 +1,23 @@
+package com.example.smartmobi;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class DicasFragment extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_dicas_layout, container, false);
+        return view;
+    }
+}
